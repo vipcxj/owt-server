846675c29aa30da51d813392a7372dc905aa5ca7
vipcxj <vipcxj@126.com>
2023-10-18 18:47:51 +0800

    push instead of pull

--- docker/components/build.sh
+++ docker/components/build.sh
@@ -3,4 +3,4 @@ set -e
 CUR_DIR=`readlink -f $(dirname $0)`
 cd $CUR_DIR
 docker build -t swr.cn-east-3.myhuaweicloud.com/sanrong/owt-component:run .
-docker pull swr.cn-east-3.myhuaweicloud.com/sanrong/owt-component:run
+docker push swr.cn-east-3.myhuaweicloud.com/sanrong/owt-component:run
